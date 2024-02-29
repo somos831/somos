2d1e34ad7baf2444483019606dc0f8f14089b716
Jose Patino <120534408+jipatino98@users.noreply.github.com>
2024-02-28 16:04:42 -0800

    Started Navbar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,14 +2,18 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import './App.css';
 import AboutUs from './layouts/AboutUs';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path='/about' Component={AboutUs} />
-      </Routes>
-    </Router>
+    <>
+      <Navbar />
+      <Router>
+        <Routes>
+          <Route path='/about' Component={AboutUs} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
